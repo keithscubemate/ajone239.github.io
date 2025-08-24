9e795b9d1607cb358214f7efbe6602dbe05aadaf
Austin Jones <ajones53.aj@gmail.com>
2025-08-24 12:35:09 -0400

    clean out some warnings

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,8 +1,12 @@
 <script>
-	import { page } from '$app/state';
+    import { page } from "$app/state";
 </script>
 
 <h1>{page.status} :(</h1>
-<p>
-	{page.error.message}
-</p>
+{#if page.error}
+    <p>
+        {page.error.message}
+    </p>
+{:else}
+    <p>An error has occured!</p>
+{/if}
--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -8,7 +8,10 @@ export const GET: RequestHandler = async () => {
     const all_posts = await fetchMarkdownPosts();
 
     const sorted_posts = all_posts.sort((a: PostEntry, b: PostEntry) => {
-        return Date.parse(b.meta.date) - Date.parse(a.meta.date)
+        const b_date = b.meta.date ?? "";
+        const a_date = a.meta.date ?? "";
+
+        return Date.parse(b_date) - Date.parse(a_date)
     });
 
     return json(sorted_posts);
