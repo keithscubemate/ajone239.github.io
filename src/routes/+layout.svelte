186b50b28cdb95b4bb8335f433e759697bcc5117
Austin Jones <ajones53.aj@gmail.com>
2025-08-24 12:23:48 -0400

    wip doesn't work use new components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,71 @@
 <script lang="ts">
-	import Header from '$lib/components/Header.svelte';
-	import Footer from '$lib/components/Footer.svelte';
-	import '$lib/styles/styles.scss';
-	import '$lib/styles/prism-styles.css';
-	import type { LayoutProps } from './$types';
+    import Footer from "$lib/components/Footer.svelte";
+    import "$lib/styles/styles.scss";
+    import "$lib/styles/prism-styles.css";
+    import type { LayoutProps } from "./$types";
+    import MobileHeader from "$lib/components/MobileHeader.svelte";
+    import DesktopHeader from "$lib/components/DesktopHeader.svelte";
+    import Sidebar from "$lib/components/Sidebar.svelte";
 
-	let { children }: LayoutProps = $props();
+    let { children }: LayoutProps = $props();
+
+    let open = $state(true);
+    let bt = $derived(open ? "c" : "o");
+
+    $inspect(open).with(console.trace);
 </script>
 
 <body>
-	<div class="site-wrapper">
-		<Header />
+    <header>
+        <div class="header-display-desktop">
+            <DesktopHeader />
+        </div>
 
-		<main>
-			<div class="main-content-wrapper">
-				{@render children()}
-			</div>
-		</main>
+        <div class="header-display-mobile">
+            <MobileHeader bind:open />
+        </div>
+    </header>
 
-		<Footer />
-	</div>
+    <div class="site-wrapper">
+        <main>
+            <button onclick={() => (open = !open)}>
+                {bt}
+            </button>
+
+            <div class="main-content-wrapper">
+                {@render children()}
+            </div>
+            <div class="header-display-mobile">
+                <Sidebar bind:open />
+            </div>
+        </main>
+    </div>
+    <Footer />
 </body>
 
 <style lang="scss">
-	.main-content-wrapper {
-		max-width: 40rem;
-		margin: 0 auto;
-		padding: 1rem;
-	}
+    .main-content-wrapper {
+        max-width: 45rem;
+        margin: 0 auto;
+        padding: 1rem;
+    }
+
+    .header-display-desktop {
+        display: flex;
+    }
+
+    .header-display-mobile {
+        display: none;
+    }
+
+    @media only screen and (pointer: coarse) and (max-width: 1024px),
+        screen and (max-width: 799px) {
+        .header-display-desktop {
+            display: none;
+        }
+
+        .header-display-mobile {
+            display: flex;
+        }
+    }
 </style>
