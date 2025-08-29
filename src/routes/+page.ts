8763ff4a27cf8b0471b93510387a15bdbd189017
Austin Jones <ajones53.aj@gmail.com>
2025-08-29 17:57:49 -0400

    Fix the build sry

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,7 +1,7 @@
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch }) => {
-    const response = await fetch(`/api/posts/latest`);
+    const response = await fetch(`/api/latest_post`);
     const post = await response.json();
 
     return { post };
